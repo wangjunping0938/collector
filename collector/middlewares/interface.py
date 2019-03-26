99ae79052e1e2f82378385d245c6df73bc97bedb
Elisha Charles <592045723@qq.com>
2019-03-26 13:11:54 +0800

    Completion of interface request function

--- collector/middlewares/interface.py
+++ collector/middlewares/interface.py
@@ -11,8 +11,7 @@ class Interface(object):
 
     def __init__(self):
         settings = get_project_settings()
-        self.webinterface  = PF.parse2dict(settings['CONFIG_FILE'],
-                                           'webinterface')
+        self.webinterface  = PF.parse2dict(settings['CONFIG_FILE'], 'webinterface')
 
     def company_list(self, field=['_id', 'name', 'number']):
         # 获取全部公司信息,过滤后只保留部分字段信息
@@ -24,7 +23,7 @@ class Interface(object):
         data = [{k:v for k, v in d.items() if k in field} for d in data]
         return data
 
-    def company_queue_list(self, status=(1, 5)):
+    def company_queue_list(self, status_name, status=(1, 5)):
         # 获取待更新队列中的公司列表
         url = self.webinterface['company_queue_list']
         parameters = {'in_grap':5}
@@ -32,33 +31,31 @@ class Interface(object):
         data = requests.get(url, parameters).json()['data']['rows']
         # 过滤不符合条件的公司
         data = [d for d in data if d['status'] == 1]
-        tyc = [d for d in data if d['tyc_status'] not in status]
-        bd = [d for d in data if d['bd_status'] not in status]
-        return tyc, bd
+        rst = [d for d in data if d[status_name] not in status]
+        return rst
 
-    def update_status(self, id, company_name, status_name, status_value):
+    def update_status(self, company, status_name, status_value):
         # 更新公司信息爬取状态
         url = self.webinterface['company_queue_submit']
-        parameters = {'id':id, status_name:status_value}
+        parameters = {'id':company['id'], status_name:status_value}
         try:
             response = requests.post(url, parameters).json()
             if response['message'] == 'success!':
-                message = '{}:爬取状态更新完成!'.format(company_name)
+                message = '{}:爬取状态更新完成!'.format(company['name'])
             else:
                 message = response['message']
             logging.info(message)
         except Exception as e:
             logging.error(e)
 
-    def updata_out_grap(self, id, number, company_name,
-                        status=['tyc_status', 'bd_status']):
+    def updata_out_grap(self, company, status=['tyc_status', 'bd_status']):
         # 根据其他爬取状态变化更新站外爬取状态值
         url = self.webinterface['company_queue_list']
-        parameters = {'in_grap':5, 'number':number}
+        parameters = {'in_grap':5, 'number':company['number']}
         response = requests.get(url, parameters).json()['data']['rows'][0]
         status = (response[status[0]], response[status[1]])
 
-        parameters = {'id':id}
+        parameters = {'id':company['id']}
         if 1 in status:
             parameters['out_grap'] = 1  # 进行中
         elif sum(status) == 10:
@@ -74,11 +71,15 @@ class Interface(object):
             response = requests.post(url, parameters).json()
             if response['message'] == 'success!':
                 message = '{}: 站外爬取状态更新完成'.format(company_name)
+                rst = True
             else:
                 message = response['message']
+                rst = False
             logging.info(message)
         except Exception as e:
             logging.error(e)
+            rst = False
+        return rst
 
     def update_data(self, data):
         # 上传爬取的公司信息数据
@@ -87,8 +88,12 @@ class Interface(object):
             response = requests.post(url, data).json()
             if response['message'] == 'success!':
                 message = '{}: 信息上传完成'.format(data['name'])
+                rst = True
             else:
                 message = response['message']
+                rst = False
             logging.info(message)
         except Exception as e:
             logging.error(e)
+            rst = False
+        return rst
