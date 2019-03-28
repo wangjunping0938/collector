3683b6a03d5bb29e39d264ce39fe920e01abbae7
Elisha Charles <592045723@qq.com>
2019-03-28 10:33:02 +0800

    Interface request process

--- collector/middlewares/interface.py
+++ collector/middlewares/interface.py
@@ -3,59 +3,70 @@
 # Data query submission interface function
 import requests
 import logging
-from collector.middlewares.parsefile import ParseFile as PF
-from scrapy.utils.project import get_project_settings
 
 
 class Interface(object):
 
-    def __init__(self):
-        settings = get_project_settings()
-        self.webinterface  = PF.parse2dict(settings['CONFIG_FILE'], 'webinterface')
+    @staticmethod
+    def company_view(url, company):
+        # 获取公司详情
+        parameters = {'number':company['number']}
+        try:
+            response = requests.get(url, parameters).json()['data']
+            return response
+        except Exception as e:
+            logging.error(e)
 
-    def company_list(self, field=['_id', 'name', 'number']):
+    @staticmethod
+    def company_list(url, field=['_id', 'name', 'number']):
         # 获取全部公司信息,过滤后只保留部分字段信息
-        url = self.webinterface['company_list']
-        parameters = {'per_page':requests.get(url).json()['data']['total_count']}
+        per_page = requests.get(url).json()['data']['total_count']
+        parameters = {'per_page':per_page}
         data = requests.get(url, parameters).json()['data']['rows']
-        # 根据条件过滤
         data = [d for d in data if d['status'] == 1]
         data = [{k:v for k, v in d.items() if k in field} for d in data]
         return data
 
-    def company_queue_list(self, status_name, status=(1, 5)):
-        # 获取待更新队列中的公司列表
-        url = self.webinterface['company_queue_list']
-        parameters = {'in_grap':5}
-        parameters['per_page'] = requests.get(url).json()['data']['total_count']
+    @staticmethod
+    def company_queue_list(url, status_name, status=(1, 5)):
+        # 获取待更新队列中的公司列表,并过滤不符合条件的公司
+        per_page = requests.get(url).json()['data']['total_count']
+        parameters = {'in_grap':5, 'per_page':per_page}
         data = requests.get(url, parameters).json()['data']['rows']
-        # 过滤不符合条件的公司
         data = [d for d in data if d['status'] == 1]
         rst = [d for d in data if d[status_name] not in status]
         return rst
 
-    def update_status(self, company, status_name, status_value):
+    @staticmethod
+    def query_status(url, company, status=['tyc_status', 'bd_status']):
+        # 查询公司信息爬取状态
+        parameters = {'in_grap':5, 'number':company['number']}
+        data = requests.get(url, parameters).json()['data']['rows'][0]
+        rst = {k:v for k, v in data.items() if k in status}
+        return list(rst.values())
+
+    @staticmethod
+    def update_status(url, company, status, value):
         # 更新公司信息爬取状态
-        url = self.webinterface['company_queue_submit']
-        parameters = {'id':company['id'], status_name:status_value}
+        parameters = {'id':company['_id'], status:value}
         try:
-            response = requests.post(url, parameters).json()
-            if response['message'] == 'success!':
-                message = '{}:爬取状态更新完成!'.format(company['name'])
+            message = requests.post(url, parameters).json()['message']
+            if message == 'success!':
+                message = '{}: update {} {}'.format(company['name'], status, message)
+                rst = True
             else:
-                message = response['message']
+                message = '{}: update {} {}'.format(company['name'], status, message)
+                rst = False
             logging.info(message)
         except Exception as e:
             logging.error(e)
+            rst = False
+        return rst
 
-    def updata_out_grap(self, company, status=['tyc_status', 'bd_status']):
+    @staticmethod
+    def updata_out_grap(url, company, status):
         # 根据其他爬取状态变化更新站外爬取状态值
-        url = self.webinterface['company_queue_list']
-        parameters = {'in_grap':5, 'number':company['number']}
-        response = requests.get(url, parameters).json()['data']['rows'][0]
-        status = (response[status[0]], response[status[1]])
-
-        parameters = {'id':company['id']}
+        parameters = {'id':company['_id']}
         if 1 in status:
             parameters['out_grap'] = 1  # 进行中
         elif sum(status) == 10:
@@ -64,16 +75,13 @@ class Interface(object):
             parameters['out_grap'] = 2  # 失败
         elif sum(status) in (0, 2, 5):
             parameters['out_grap'] = 0  #未抓取
-
-        # 更新站外爬取状态
-        url = self.webinterface['company_queue_submit']
         try:
-            response = requests.post(url, parameters).json()
-            if response['message'] == 'success!':
-                message = '{}: 站外爬取状态更新完成'.format(company_name)
+            message = requests.post(url, parameters).json()['message']
+            if message == 'success!':
+                message = '{}: update out_grap {}'.format(company['name'], message)
                 rst = True
             else:
-                message = response['message']
+                message = '{}: update out_grap {}'.format(company['name'], message)
                 rst = False
             logging.info(message)
         except Exception as e:
@@ -81,9 +89,9 @@ class Interface(object):
             rst = False
         return rst
 
-    def update_data(self, data):
+    @staticmethod
+    def update_data(url, data):
         # 上传爬取的公司信息数据
-        url = self.webinterface['company_update']
         try:
             response = requests.post(url, data).json()
             if response['message'] == 'success!':
@@ -97,3 +105,31 @@ class Interface(object):
             logging.error(e)
             rst = False
         return rst
+
+
+if __name__ == '__main__':
+    opalus_company_list = 'http://opalus-dev.taihuoniao.com/api/design_company/list'
+    opalus_queue_list = 'http://opalus-dev.taihuoniao.com/api/company_queue/list'
+    opalus_queue_submit = 'http://opalus-dev.taihuoniao.com/api/company_queue/submit'
+    opalus_company_view = 'http://opalus-dev.taihuoniao.com/api/design_company/view'
+    opalus_company_update = 'http://opalus-dev.taihuoniao.com/api/design_company/update'
+    opalus_image_submit = 'http://opalus-dev.taihuoniao.com/api/image/submit'
+
+    cpys = Interface.company_list(opalus_company_list)
+
+    company = {'_id': '5a951f5db24b006f0fe723f2', 'name': '北京上品极致产品设计有限公司', 'number': 180227170533300348}
+    cpyv = Interface.company_view(opalus_company_view, company)
+    print(cpyv)
+
+    cqltyc = Interface.company_queue_list(opalus_queue_list, 'tyc_status')
+    print(cqltyc)
+    cqlbd = Interface.company_queue_list(opalus_queue_list, 'bd_status')
+    print(cqlbd)
+
+    company = {'_id':'5b3b3271b24b005a46d493fc', 'name':'嘉兰图设计股份有限公司', 'number':180621164746444014}
+    Interface.update_status(opalus_queue_submit, company, 'bd_status', 0)
+    Interface.update_status(opalus_queue_submit, company, 'tyc_status', 0)
+
+    cqlc = Interface.query_status(opalus_queue_list, company)
+    print(cqlc)
+    Interface.updata_out_grap(opalus_queue_submit, company, cqlc)
